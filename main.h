33800e75d9a9b353caa4785f82794c014f5da3e7
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-03-26 20:16:31 +0100

    header file

--- main.h
+++ main.h
@@ -1,8 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+
+/**
+ * struct print_t - structure to define corresponding function to print.
+ * @c: the type to print.
+ * @func: the function associated to the type.
+ */
+
+typedef struct print_t
+{
+	char *c;
+	int (*func)(va_list);
+} pr_t;
+
 int _printf(const char *format, ...);
-char *_strrev (char *str);
-char * _itoa(int i, char *strout, int base);
+int pr_char(va_list c);
+int pr_string(va_list str);
 
 #endif
